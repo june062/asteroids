02a32294e80b8158385e3faa4eaf1b326c2454b2
Juneid Teklu <te.juneid@gmail.com>
2025-03-03 19:44:39 -0600

    Add movement functionality

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from player import *
 
 def main():
     pygame.init()
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -18,7 +21,10 @@ def main():
         
         dt = clock.tick(60) / 1000
         screen.fill((0,0,0))
-        player.draw(screen)
+        updatables.update(dt)
+        for drawable in drawables:
+            drawable.draw(screen)
+        
         pygame.display.flip()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
